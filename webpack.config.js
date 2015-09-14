070c61dc9684ee246218c498c0ba1e5ca5b12eff
Steven Dickinson <sd@totemlegal.co.uk>
2015-09-14 09:37:17 +0100

    Make react a non-external dependency. Bloats the dist package but I've moved on from that.

--- webpack.config.js
+++ webpack.config.js
@@ -23,17 +23,6 @@ module.exports = {
         libraryTarget: 'umd'
     },
 
-    externals: [
-        {
-            "react": {
-                root: "React",
-                commonjs2: "react",
-                commonjs: "react",
-                amd: "react"
-            }
-        }
-    ],
-
     node: {
         Buffer: false
     },
