c63b19838802315f30e3ae92ff65ed020e562cb4
Steven Dickinson <sd@tonic.works>
2018-05-21 17:32:09 +0100

    Replace createclass

--- module/component/ActionLink.js
+++ module/component/ActionLink.js
@@ -3,12 +3,10 @@
 var React = require('react'),
     Generate = require('../Url').Generate;
 
-var ActionLink = React.createClass({
-    render: function() {
-        return React.createElement('a', {
-            href: Generate(this.props.routeName, this.props.params)
-        }, this.props.children);
-    }
-});
+function ActionLink(props) {
+    return React.createElement('a', {
+        href: Generate(this.props.routeName, this.props.params)
+    }, this.props.children);
+}
 
 module.exports = ActionLink;
